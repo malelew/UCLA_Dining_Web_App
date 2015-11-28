90b037c2d9ac6610564af7a7fe10f47d6b1eca19
malelew@ucsd.edu <SoloMune@Mos-MacBook-Pro.local>
2015-11-28 15:40:26 -0800

    main flask file and the virtualenv

---
+++ uclafood.py
@@ -0,0 +1,20 @@
+import os 
+#import psycopg2
+import urlparse 
+#module for building webapps
+from flask import Flask, url_for, render_template, request
+import requests
+#module for processing html and xml 
+import bs4
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "hello"
+
+if __name__ == "__main__":
+  app.run()
+
+
